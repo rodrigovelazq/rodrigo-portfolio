fcf37aa07980bd2fbdb41d954746ccb9160bd594
rodrigovelazq <rodrigo.24.06.90@gmail.com>
2023-03-02 11:42:45 -0300

    First commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/rodrigovelazq/rodrigo-portfolio'
+      className='link footer__link'
+    >
+      Created By Rodrigo Velázquez
+    </a>
+  </footer>
+)
+
+export default Footer
